14c54203c85b6d5f5251d3291971452794e213e3
Jonghwi <ellia2431@naver.com>
2025-04-11 14:04:15 +0900

    cycle7: add Of12 test

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,23 +6,13 @@ public:
 		std::vector<int> result = {};
 		if (num > 1) {
 			int divisor = 2;
-			if (num == 4) {
-				while (num % divisor == 0) {
-					result.push_back(divisor);
-					num /= divisor;
-				}
-			}
-			else if (num == 6) {
+			if (num == 4 || num == 6 || num == 9 || num == 12) {
 				for (divisor = 2; num > 1; divisor++) {
 					while (num % divisor == 0) {
 						result.push_back(divisor);
 						num /= divisor;
 					}
 				}
-			}
-			else if (num == 9) {
-				result.push_back(3);
-				result.push_back(3);
 			}
 			else
 				result.push_back(num);
