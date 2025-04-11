0b78f3ed090b5049b03528ef908b2b9e14ad0af2
Jonghwi <ellia2431@naver.com>
2025-04-11 13:59:07 +0900

    cycle5: add Of6 test

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,8 +6,14 @@ public:
 		std::vector<int> result = {};
 		if (num > 1) {
 			if (num == 4) {
+				while (num % 2 == 0) {
+					result.push_back(2);
+					num /= 2;
+				}
+			}
+			else if (num == 6) {
 				result.push_back(2);
-				result.push_back(2);
+				result.push_back(3);
 			}
 			else
 				result.push_back(num);
