fa0186fa388cdb6e487215ee678d1df9011e37b2
Jonghwi <ellia2431@naver.com>
2025-04-11 13:57:18 +0900

    cycle4: add Of4 test

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -4,11 +4,13 @@ class PrimeFactor {
 public:
 	std::vector<int> of(int num) {
 		std::vector<int> result = {};
-		if (num == 2) {
-			result.push_back(num);
-		}
-		else if (num == 3) {
-			result.push_back(num);
+		if (num > 1) {
+			if (num == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else
+				result.push_back(num);
 		}
 		return result;
 	}
